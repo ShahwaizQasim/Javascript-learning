4e73441c999fd60c8b0ca9622180a8a89f5fa944
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-02-20 12:50:46 +0500

    javascript learning

---
+++ app.js
@@ -0,0 +1,19 @@
+let name = "my name is shahwaiz"
+console.log(name);
+
+let num = 5 + 4
+console.log(num);
+
+let myname = "Syed Shahwaiz"
+console.log("My name is " + myname);
+
+console.log( 5 - 3)
+
+const pi = 3.142
+console.log(pi)
+
+let a = parseInt(prompt("1 value"))
+let b = parseInt(prompt("2 value"))
+let c = a + b
+alert(c)
+// console.log(a + b);
